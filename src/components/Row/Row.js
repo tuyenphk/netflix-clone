9a988a9d9fef799f283c8667cbb3e368d26ee6a4
Tuyen Pham <tuh39709@temple.edu>
2021-04-07 16:14:29 -0400

    movies row posters

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import './Row.css'
 import axios from '../../axios'
 
+const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
 
@@ -15,14 +16,20 @@ function Row({title, fetchUrl}) {
             return request;
         }
         fetchData();
-    }, [])
+    }, [fetchUrl])
+
+    console.log(movies)
 
     return (
-        <div>
-            {/* title */}
+        <div className="row">
             <h2>{title}</h2>
 
-            {/* containers -> posters */}
+            <div className="row-posters">
+                {movies.map(movie => {
+                    return <img className="row-poster"
+                                src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                })}
+            </div>
             
         </div>
     )
