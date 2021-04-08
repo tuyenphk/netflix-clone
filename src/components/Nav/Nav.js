ff8190d2ed2271c74418827af3c69f846bd243eb
Tuyen Pham <tuh39709@temple.edu>
2021-04-08 17:02:35 -0400

    handle nav header

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Nav.css'
 
 function Nav() {
+    const [show, handleShow] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100){
+                handleShow(true);
+            } else handleShow(false)
+        });
+        return () => {
+            window.removeEventListener("scroll"); 
+        }
+    }, [])
+
     return (
-        <div className="nav">
+        <div className={`nav ${show && "nav-black"}`}>
             <img
                 className="nav-logo"
                 src="https://i.ibb.co/3sdkQzW/1.png"
