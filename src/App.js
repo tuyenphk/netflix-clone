f3e20a9e6f5a89279de69a3cb883d6e2612bd890
Tuyen Pham <tuh39709@temple.edu>
2021-04-07 20:54:32 -0400

    update css of Row

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Row 
         title="NETFLIX ORIGINALS" 
         fetchUrl={requests.fetchNetflixOriginals} 
-        isLargeRow
+        isLargeRow={true}
       />
       <Row title="Trending now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
