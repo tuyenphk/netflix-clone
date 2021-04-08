b2782a70f51cef96cdcf77f6560eb31bc7bcaa8f
Tuyen Pham <tuh39709@temple.edu>
2021-04-08 14:15:47 -0400

    banner info displayed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,6 +3,7 @@ import './Banner.css'
 import axios from '../../axios'
 import requests from '../../requests'
 
+const base_url = "https://image.tmdb.org/t/p/original/";
 function Banner() {
     const [movie, setMovie] = useState([])
 
@@ -20,9 +21,25 @@ function Banner() {
     console.log(movie)
 
     return (
-        <div>
-            
-        </div>
+        <header className="banner" 
+                style={{
+                    backgroundSize: "cover",
+                    backgroundImage: `url('${base_url}${movie?.backdrop_path}')`,
+                    backgroundPosition: "center center"
+                }}>
+            <div className="banner-content">
+                <h1 className="banner-title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                <div className="banner-buttons">
+                    <button className="banner-button">Play</button>
+                    <button className="banner-button">My List</button>
+                </div>
+                <p className="banner-description">
+                    {movie?.overview}
+                </p>
+            </div>
+        </header>
     )
 }
 
